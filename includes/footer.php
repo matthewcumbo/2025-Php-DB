fe4f7ec55dea23ce570d68881d80564b0f206234
matthewcumbo <matthew.abraham.cumbo@mcast.edu.mt>
2025-11-13 08:56:08 +0100

    Setup main struture using includes for header and footer

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<!-- 
+    This is a partial page, used for the footer.
+    This page will be included in every other page to have
+    the same footer everywhere.
+-->
+<div class="bg-warning">
+        &copy; Matthew Cumbo - 2025
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
