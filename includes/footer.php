80532a543ad8e88f0b68151e9ed03525cbf045cb
matthewcumbo <matthew.abraham.cumbo@mcast.edu.mt>
2025-12-01 09:56:36 +0100

    Prep for Register/Edit

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,8 @@
     This page will be included in every other page to have
     the same footer everywhere.
 -->
-<div class="bg-warning">
-        &copy; Matthew Cumbo - 2025
+<div class="bg-warning d-flex justify-content-center fixed-bottom" style="height:50px;">
+        <p class="h-100 m-0 d-flex align-items-center">&copy; Matthew Cumbo - 2025</p> 
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
---
+++ register.php
@@ -0,0 +1,68 @@
+<?php 
+    include "includes/header.php";
+
+?>
+
+<div class="container" style="width:800px;">
+    <div class="row">
+        <div class="col">
+            <h3>Register</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <form action="" method="post">
+                <div class="row">
+                    <div class="col">
+                        <input type="text" name="username" id="username" placeholder="Username" class="w-100 m-2">
+                    </div>
+                    <div class="col">
+                        <input type="password" name="password" id="password" placeholder="Password" class="w-100 m-2">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <input type="text" name="name" id="name" placeholder="First Name" class="w-100 m-2">
+                    </div>
+                    <div class="col">
+                        <input type="text" name="surname" id="surname" placeholder="Last Name" class="w-100 m-2">
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col w-100 m-2">
+                        <label for="age" class="float-end">Age:</label>
+                    </div>
+                    <div class="col w-100 m-2">
+                        <select name="age" id="age">
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row my-3">
+                    <div class="col">
+                        <button class="btn btn-success w-100 m-2" type="submit">Submit</button>
+                    </div>
+                    <div class="col">
+                        <button class="btn btn-danger w-100 m-2" type="reset">Cancel</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php include "includes/footer.php"; ?>
